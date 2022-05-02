9b93e3243a25a3fab208eb2e469397efb0ce50f9
Prashant kumar <94132841+Prsh8045@users.noreply.github.com>
2022-05-02 13:30:24 +0530

    Add files via upload

---
+++ src/Sdata.jsx
@@ -0,0 +1,38 @@
+const Sdata=[
+    {
+        imgsrc:
+          "https://i.pinimg.com/564x/bd/9b/e9/bd9be9cfc97ba73c66f51e927f2bfcc5.jpg",
+        title: "A Netflix Original Series",
+        sname: "DARK",
+        links: "https://www.netflix.com/in/title/80100172",
+      },
+      {
+        imgsrc:
+          "https://news.iium.edu.my/wp-content/uploads/2020/05/Extracurricular-netflix.jpg",
+        title: "A Netflix Original Series",
+        sname: "EXTRA CURRICULAR",
+        links: "https://www.netflix.com/in/title/80990668",
+      },
+      {
+        imgsrc: "https://i.ytimg.com/vi/gM5OVZtq8xM/maxresdefault.jpg",
+        title: "A Netflix Original Series",
+        sname: "STRANGER THINGS",
+        links: "https://www.netflix.com/in/title/80057281",
+      },
+      {
+        imgsrc:
+          "https://m.media-amazon.com/images/M/MV5BMDk3YzgxNDQtNTEzOS00NDMyLWFlYmYtYTZlMDk1NDkxNmMyXkEyXkFqcGdeQXVyNzA5NjUyNjM@._V1_FMjpg_UX1000_.jpg",
+        title: "A Netflix Original Series",
+        sname: "THE VAMPIRE DIARIES",
+        links: "https://www.netflix.com/in/title/70143860",
+      },
+      {
+        imgsrc:
+          "https://6.viki.io/image/514d16ceea7446eeb9d77f6d61405ccb.jpeg?s=900x600&e=t",
+        title: "A Netflix Original Series",
+        sname: "MY FIRST FIRST LOVE",
+        links: "https://www.netflix.com/in/title/81026700",
+      },
+
+];
+export default Sdata;
